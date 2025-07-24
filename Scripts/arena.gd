5aea6550e8c1d953692c6102a2e9ffb1ca20c27d
Gericault <Calefrend@live.com.au>
2025-07-24 21:39:03 +0930

    Implement unit selling functionality
    logic for selling units through the sell portal.
    
    - Implemented the `_sell_unit` function to update player gold and remove the unit from the game.
    - Added event connections for unit dropping and entering the portal.
    - Basic UI elements (gold display and hide/show)

--- Scripts/arena.gd
+++ Scripts/arena.gd
@@ -6,8 +6,10 @@ const HALF_CELL_SIZE := Vector2(16, 16)
 const QUARTER_CELL_SIZE := Vector2(8, 8)
 
 @onready var unit_mover: UnitMover = $UnitMover
-@onready var unit_spawner: UnitSpawner = $unit_spawner
+@onready var unit_spawner: UnitSpawner = $UnitSpawner
+@onready var sell_portal: SellPortal = $SellPortal
 
 #plug in unit spawned and connect it to drag and drop component
 func _ready() -> void:
-    unit_spawner.unit_spawned.connect(unit_mover.setup_unit)
+	unit_spawner.unit_spawned.connect(unit_mover.setup_unit)
+	unit_spawner.unit_spawned.connect(sell_portal.setup_unit)
--- data/units/unit_stats.gd
+++ data/units/unit_stats.gd
@@ -15,10 +15,24 @@ const RARITY_COLOURS := {
 @export_category("Data")
 @export var rarity: Rarity
 @export var gold_cost := 1
+@export_range(1, 3) var tier := 1 : set = _set_tier
 
 @export_category("Visuals")
 # use coords because I am using a sprite sheet and only need coordinates to draw the unit
 @export var skin_coordinates: Vector2i
+
+# sell cost is 1:1; 3 units to combine to the power of tier - 1
+# ie tier 1 (no combine) is 0. tier 2 is to the power of 1 (3x3)
+# and so on
+func _get_combined_unit_count() -> int:
+    return 3 **(tier-1)
+
+func _get_gold_value() -> int:
+    return gold_cost * _get_combined_unit_count()
+
+func _set_tier(value: int) -> void:
+    tier = value
+    emit_changed()
 
 func _to_string() -> String:
     return name
