190c0a22e7bed2ade5efd2ec43ff2b0be4969f95
Gericault <Calefrend@live.com.au>
2025-07-27 20:30:35 +0930

    Added Shop
    Added shop, unit cards, reroll and XP buy
    updated player_stats

--- data/player/player_stats.gd
+++ data/player/player_stats.gd
@@ -1,10 +1,27 @@
 class_name PlayerStats
 extends Resource
 
+const MAX_LEVEL = 10
+const XP_REQUIREMENTS := {
+    1:0,
+    2: 2,
+    3: 2,
+    4: 6,
+    5: 10,
+    6: 20,
+    7: 36,
+    8: 48,
+    9: 76,
+    10: 76,
+}
+
 @export_range(0, 99) var gold: int : set = _set_gold
 @export_range(0, 99) var xp: int : set = _set_xp
 @export_range(1, 10) var level: int : set = _set_level
 
+func get_current_xp_requiement() -> int:
+    var next_level = clamp(level+1, 1, MAX_LEVEL)
+    return XP_REQUIREMENTS[next_level]
 
 func _set_gold(value: int) -> void:
     gold = value
@@ -13,6 +30,18 @@ func _set_gold(value: int) -> void:
 func _set_xp(value: int) -> void:
     xp = value
     emit_changed()
+
+    if level == MAX_LEVEL:
+        return
+    
+    var xp_requirement: int = get_current_xp_requiement()
+
+    # levelling is a loop in case of handling multiple level ups
+    while level < MAX_LEVEL and xp >= xp_requirement:
+        level += 1
+        xp -= xp_requirement
+        xp_requirement = get_current_xp_requiement()
+        emit_changed()
 
 func _set_level(value: int) -> void:
     level = value
